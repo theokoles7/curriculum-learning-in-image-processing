4607cd576013c085ab1e72accb416523b5592683
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2024-07-06 17:33:43 -0500

    Fixes -- -> False

--- commands/init_results.py
+++ commands/init_results.py
@@ -52,7 +52,7 @@ def init_results() -> None:
                                     model,
                                     dataset,
                                     curriculum,
-                                    "--",
+                                    False,
                                     epochs,
                                     batch_size,
                                     "--", "--", "--", "--", "--", "--"
